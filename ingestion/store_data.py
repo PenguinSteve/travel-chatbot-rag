423dca7069442af95aaeba2a272a649895511065
NToan324 <nhattoan664t@gmail.com>
2025-11-07 17:15:40 +0700

    update

--- ingestion/store_data.py
+++ ingestion/store_data.py
@@ -85,21 +85,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def main():
     CONNECTION_STRING = f"mongodb+srv://{settings.MONGO_DB_NAME}:{settings.MONGO_DB_PASSWORD}@chat-box-tourism.ojhdj0o.mongodb.net/?retryWrites=true&w=majority&tls=true"
-
     script_path = os.path.abspath(__file__)
 
     script_dir = os.path.dirname(script_path)
 
     project_root = os.path.dirname(script_dir)
-
-    file_path = os.path.join(project_root, "data", "data_additional.xlsx")
+    
+    file_path = os.path.join(project_root, "data", "data_tourism_TPHCM.xlsx")
     
     docstore = MongoDBStore(
         connection_string=CONNECTION_STRING,
         db_name=settings.MONGO_DB_NAME,
         collection_name=settings.MONGO_STORE_COLLECTION_NAME
     )
-
     vector_store = PineconeConfig().get_vector_store()
 
     documents = DataService.load_raw_data(filepath=file_path)
